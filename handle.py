9d197f95abc35f299205516f4be6d739cc965e62
Angus <1621xjx@buaa.edu.cn>
2018-07-18 16:43:57 +0800

    Initialize

---
+++ handle.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+# filename: handle.py
+import hashlib
+import reply
+import receive
+import media
+import web
+import ssl
+import urllib.request
+import urllib.parse
+import re
+
+class Handle(object):
+    def GET(self):
+            data = web.input()
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr
+            token = "wechat" #请按照公众平台官网\基本配置中信息填写
+
+            list = [token, timestamp, nonce]
+            list.sort()
+            list2 = ''.join(list)
+            sha1 = hashlib.sha1()
+            sha1.update(list2.encode('utf-8'))
+            hashcode = sha1.hexdigest()
+            print(str(hashcode) + " " + str(signature) + " " + str(echostr))
+            if hashcode == signature:
+                return echostr  # 返回echostr
+
+    def download(url, local_filename):
+        r = requests.get(url, stream=True)
+        with open(os.getcwd()+"/"+local_filename, 'wb') as f:
+            for chunk in r.iter_content(1024):
+                if chunk:
+                   f.write(chunk)
+                   f.flush()
+        return local_filename
+
+    def POST(self):
+        try:
+            webData = web.data()
+            print ("Handle Post webdata is ", webData)
+   #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                input = recMsg.Content.decode(encoding='utf-8')
+                # context = ssl._create_unverified_context()
+                txt = input.split()
+                if txt[0] == "ins":
+                    url = txt[1]
+                    r = requests.get(url, params={'__a': 1})
+                    if (
+                        (r.headers['content-type'] != 'application/json') or
+                        (not 'graphql' in r.json())
+                    ):
+                        raise Exception('Wrong link')
+
+                    media = r.json()['graphql']['shortcode_media']
+                    if media['is_video']:
+                        print('Saved as ' + download(media['video_url'],
+                                                     media['shortcode'] + '.mp4') + '!')
+                        myMedia = Media()
+                        accessToken = Basic().get_access_token()
+                        filePath = media['shortcode'] + '.mp4'   #请安实际填写
+                        mediaType = "video"
+                        link = myMedia.uplaod(accessToken, filePath, mediaType).MediaID
+                    else:
+
+                        if media.get('edge_sidecar_to_children',None):
+                            link = 'You should send a link of picture.'
+                            print(link)
+                            replyMsg = reply.TextMsg(toUser, fromUser, link)
+                            return replyMsg.send()
+                            # print('Downloading mutiple images of this post')
+                            # for child_node in media['edge_sidecar_to_children']['edges']:
+                            #     print('Saved as ' + download(child_node['node']['display_url'],
+                            #                                  child_node['node']['shortcode'] + '.jpg') + '!')
+                        else:
+                            print('Saved as ' + download(media['display_url'],
+                                                         media['shortcode'] + '.jpg') + '!')
+                            myMedia = Media()
+                            accessToken = Basic().get_access_token()
+                            filePath = media['shortcode'] + '.mp4'   #请安实际填写
+                            mediaType = "video"
+                            link = myMedia.uplaod(accessToken, filePath, mediaType).MediaID            
+                # output = urllib.request.urlopen(
+                #     "https://tagging.aminer.cn/query/" + input,
+                #     context=context).read().decode(encoding='utf-8')
+                # print(output)
+                # dic = re.findall("\"([^\"]*)\": \"([^\"]*)\"", output)
+                # url = "https://www.aminer.cn/search?"
+                # ans = ""
+                # for info in dic:
+                #     if info[1] == "org":
+                #         url += "o=" + urllib.parse.quote(info[0]) + "&"
+                #         ans += "来自" + info[0]
+                # for info in dic:
+                #     if info[1] == "name":
+                #         url += "n=" + urllib.parse.quote(info[0]) + "&"
+                #         ans += "名为" + info[0] + "的学者"
+                # for info in dic:
+                #     if info[1] == "item":
+                #         url += "k=" + urllib.parse.quote(info[0]) + "&"
+                #         ans += "的" + info[0]
+                # url = url[:-1]
+                # link = "<a href=\"" + url + "\">" + ans + "</a>"
+                # print(link)
+                # replyMsg = reply.TextMsg(toUser, fromUser, link)
+                replyMsg = reply.ImageMsg(toUser, fromUser, link)
+                return replyMsg.send()
+            else:
+                if recMsg.MsgType == 'image':
+                    print(recMsg.MediaId)
+                print ("暂且不处理")
+                return "success"
+        except Exception as Argment:
+            return Argment
+
+
